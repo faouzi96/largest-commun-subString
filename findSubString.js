ab360fd07c484612fa709b8707099a94d28f4ef8
Faouzi BOUSSAD <80760013+faouzi96@users.noreply.github.com>
2022-05-30 01:16:04 +0200

    Update

--- findSubString.js
+++ findSubString.js
@@ -1,59 +1,38 @@
-/**
- * 
- * @param {String} arg1 
- * @param {String} arg2 
- * @returns String : The longest common substring between 2 Strings
- */
- function largestSubString(arg1, arg2){ 
+
+ function getAllCase(arg1){ 
     
     let subStrings = []
  
-    for(let i=0; i< arg2.length; i++){
-       for (let j=0; j < arg2.length; j++){
-           const subStr = arg2.slice(i,arg2.length-j)
-           if(arg1.indexOf(subStr) !== -1){
-                 subStrings.push(subStr)
-           }
+    for(let i=0; i< arg1.length; i++){
+       for (let j=0; j < arg1.length; j++){
+        if(arg1.slice(i,arg1.length-j).length !== 0) subStrings.push(arg1.slice(i,arg1.length-j))
        }   
     }
-    for(let i=0; i< arg2.length; i++){
-       for (let j=0; j < arg2.length; j++){
-           const subStr = arg2.slice(j,arg2.length-i)
-           if(arg1.indexOf(subStr) !== -1){
-                subStrings.push(subStr)
-           }
-       }   
-    }
- 
-     let substr = subStrings[0]
-     for(let i=1; i< subStrings.length; i++){
-        if(subStrings[i].length > substr.length) substr = subStrings[i] 
-     }
- 
-     return substr
+    return subStrings;
   }
  
+ 
   
- function findSubString(...args){
-    let subStringsArray = []
- 
-    if(args.length === 0) return "No argument"
-    if(args.length === 1) return args[0]
-    else {
-        for(let i=0; i< args.length; i++){
-            for(let j=i+1; j< args.length; j++){
-              subStringsArray.push(largestSubString(args[i],args[j]))
-            }
+ function task1(){
+    let args
+    let resultArr = []
+    if(process.argv.length <= 2) return("")
+    else args = process.argv.slice(2)
+    if(args.length === 1) return(args[0])
+    const arrayPossiblity = getAllCase(args[0])
+    for (let i=0; i < arrayPossiblity.length; i++){
+        let x=0
+        for (let j=1; j < args.length; j++){
+            if(args[j].indexOf(arrayPossiblity[i]) !== -1) x++
         }
+        if(x === args.length -1) resultArr.push(arrayPossiblity[i])
     }
- 
-    result = subStringsArray[0]
-    for(let i=1; i< subStringsArray.length; i++){
-       if(subStringsArray[i].length < result.length) result = subStringsArray[i]
+    let result = resultArr[0]
+    for(let i=1; i< resultArr.length; i++){
+       result = resultArr[i].length > result.length ? resultArr[i] : result 
     }
- 
+    if(!result) return ""
     return result
- }
+  }
  
- console.log(findSubString())
- 
+ console.log(task1())
